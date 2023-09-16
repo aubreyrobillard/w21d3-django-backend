0e1bdf9a2588e25478d6f42da02d64f4dbce4537
Aubrey Robillard <aubreyrobillard@Aubreys-MacBook-Pro.local>
2023-09-16 11:38:12 -0500

    todo project established, model created

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!.us.bin.env bash
+
+# exit when any command fails
+set o errexit
+
+# install dependencies via pip
+pip3 install -r dependencies
+
+# Run migration just in case
+python3 manage.py migrate
